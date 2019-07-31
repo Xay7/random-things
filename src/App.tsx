6afbf97299529a94a5d4fef3adb607c4120970bd
Emil <emilian.cw@gmail.com>
2019-07-31 22:24:53 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faGamepad,
-  faTv,
-  faFilm,
-  faInfinity,
-  faSearch
-} from "@fortawesome/free-solid-svg-icons";
+import "./app.css";
+import styled from "@emotion/styled";
+import { faGamepad, faTv, faFilm, faSearch, faRandom } from "@fortawesome/free-solid-svg-icons";
+import { Switch, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+library.add(faGamepad, faTv, faFilm, faSearch, faRandom);
+const Movie = lazy(() => import("./components/Elements/Movie/Movie"));
+const Game = lazy(() => import("./components/Elements/Game/Game"));
+const Television = lazy(() => import("./components/Elements/Television/Television"));
+const Number = lazy(() => import("./components/Elements/Number/Number"));
 
-library.add(faGamepad, faTv, faFilm, faInfinity, faSearch);
+const Container = styled.main`
+  margin: 0 auto;
+  width: 65vw;
+  height: 100vh;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="app">
+    <>
       <Navbar />
-      <Main />
-    </div>
+      <Container>
+        <Suspense fallback={<div>AOSDJKASOIPDJASDOIPASDJIASDIOPASDJIOPASOIJD</div>}>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/game" component={Game} />
+            <Route exact path="/movie" component={Movie} />
+            <Route exact path="/tv show" component={Television} />
+            <Route exact path="/number" component={Number} />
+          </Switch>
+        </Suspense>
+      </Container>
+    </>
   );
 };
 
